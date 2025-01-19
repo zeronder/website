324aa84a2564c16cd3eef59ef6f0832ffb8a222c
zeronder <rohitsaini838797@gmail.com>
2025-01-19 16:23:19 +0530

    rk

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# @app.route("/generatefile", methods=['GET'])
+# def generate_file():
+#     filepath = 'genefile.txt'
+#     content = "this is generage ifle"
+#     with open(filepath, 'w') as file:
+#         file.write(content)
+#     return "<p>file generated successfully</p>"
 
 if __name__ == "__main__":
     app.run() # this is commit
